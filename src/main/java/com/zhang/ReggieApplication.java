5d977e46a1e0eef1287e0ee47d4b9dbbf01fb977
Allen <Allen@163.com>
2022-07-29 09:19:53 +0800

    使用Spring Cache 缓存套餐数据

--- src/main/java/com/zhang/ReggieApplication.java
+++ src/main/java/com/zhang/ReggieApplication.java
@@ -3,8 +3,11 @@ package com.zhang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+//开启注解缓存功能
+@EnableCaching
 //开启组件扫描
 @ServletComponentScan
 @SpringBootApplication
--- src/main/java/com/zhang/controller/SetmealController.java
+++ src/main/java/com/zhang/controller/SetmealController.java
@@ -12,7 +12,8 @@ import com.zhang.service.SetmealDishService;
 import com.zhang.service.SetmealService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,6 +40,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto){
 
         setmealService.saveWithDish(setmealDto);
@@ -101,13 +103,10 @@ public class SetmealController {
      */
 
     @DeleteMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids){
-
         setmealService.removeWithDish(ids);
-
-
         return R.success("成功删除");
-
     }
 
     /**
@@ -136,6 +135,7 @@ public class SetmealController {
      * @return
      */
     @GetMapping("/list")
+    @Cacheable(value = "setmealCache",key = "#setmeal.categoryId + '_' + #setmeal.status")
     public R<List<Setmeal>> list(Setmeal setmeal){
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(setmeal.getCategoryId() != null,Setmeal::getCategoryId,setmeal.getCategoryId());
